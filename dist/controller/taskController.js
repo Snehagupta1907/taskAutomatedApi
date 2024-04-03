d1d4f94a7a92b6a04f85b33a49717c060745b04c
Snehagupta1907 <snehagupta98930@gmail.com>
2024-04-03 18:14:07 +0530

    agenda

--- dist/controller/taskController.js
+++ dist/controller/taskController.js
@@ -1,4 +1,7 @@
 "use strict";
+// import { Request, Response } from "express";
+// import Task,{ITask} from "../models/Task";
+// import axios, { AxiosResponse } from "axios";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,10 +15,55 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTasksByUserToken = exports.getTasksByStatus = exports.executeTask = exports.createTask = void 0;
+exports.getTasksByUserToken = exports.getTasksByStatus = exports.createTask = exports.executeTask = void 0;
 const Task_1 = __importDefault(require("../models/Task"));
 const axios_1 = __importDefault(require("axios"));
-//task creation post endpoint
+const agenda_1 = __importDefault(require("agenda"));
+const agenda = new agenda_1.default({ db: { address: process.env.MONGODB_URI } });
+const executeTask = (task) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield (0, axios_1.default)({
+            method: task.method,
+            url: task.endpoint,
+            headers: { "Content-Type": "application/json" },
+            data: task.data,
+        });
+        let taskStatus;
+        if (response.status === 200) {
+            task.status = "complete";
+            taskStatus = "complete";
+        }
+        else {
+            task.status = "failed";
+            taskStatus = "failed";
+        }
+        yield task.save();
+        return { responseData: response.data, taskStatus };
+    }
+    catch (err) {
+        console.error("Error executing task:", err);
+        task.status = "failed";
+        yield task.save();
+        return { responseData: null, taskStatus: "failed" };
+    }
+});
+exports.executeTask = executeTask;
+agenda.define("process task", (job) => __awaiter(void 0, void 0, void 0, function* () {
+    const task = yield Task_1.default.findById(job.attrs.data.taskId);
+    if (!task) {
+        console.error("Task not found");
+        return null;
+    }
+    const { responseData, taskStatus } = yield (0, exports.executeTask)(task);
+    console.log(responseData, taskStatus, "stat");
+    if (taskStatus === "complete") {
+        console.log("Task is done");
+    }
+}));
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield agenda.start();
+}))();
+// Task creation endpoint
 const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { endpoint, delay, method } = req.query;
     const { to, subject, text } = req.body;
@@ -39,50 +87,18 @@ const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
         const task = new Task_1.default(taskData);
         yield task.save();
         console.log("Task saved");
-        const { responseData, taskStatus } = yield (0, exports.executeTask)(task);
-        res.status(200).json({ responseData, status: taskStatus });
+        const scheduledTime = new Date(Date.now() + task.delay);
+        yield agenda.schedule(scheduledTime, "process task", { taskId: task._id });
+        res.status(200).json({
+            message: "Task scheduled successfully",
+            taskId: task._id,
+        });
     }
     catch (err) {
         res.status(400).send(err.message);
     }
 });
 exports.createTask = createTask;
-const executeTask = (task) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield new Promise((resolve) => setTimeout(resolve, task.delay));
-        const reqData = {
-            method: task.method,
-            url: task.endpoint,
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        if (task.method.toLowerCase() !== "get") {
-            reqData.data = task.data;
-        }
-        const response = yield (0, axios_1.default)(reqData);
-        console.log(response.data, "response data");
-        let taskStatus;
-        if (response.status === 200) {
-            task.status = "complete";
-            taskStatus = "complete";
-        }
-        else {
-            task.status = "failed";
-            taskStatus = "failed";
-        }
-        yield task.save();
-        return { responseData: response.data, taskStatus };
-    }
-    catch (err) {
-        console.error("Error executing task:", err);
-        task.status = "failed";
-        yield task.save();
-        return { responseData: null, taskStatus: "failed" };
-    }
-});
-exports.executeTask = executeTask;
-// get task status
 const getTasksByStatus = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { status } = req.params;
     try {
@@ -94,7 +110,6 @@ const getTasksByStatus = (req, res) => __awaiter(void 0, void 0, void 0, functio
     }
 });
 exports.getTasksByStatus = getTasksByStatus;
-//get task status by user token
 const getTasksByUserToken = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const user = req.user;
--- src/controller/taskController.ts
+++ src/controller/taskController.ts
@@ -1,66 +1,130 @@
+// import { Request, Response } from "express";
+// import Task,{ITask} from "../models/Task";
+// import axios, { AxiosResponse } from "axios";
+
+// //task creation post endpoint
+// export const createTask = async (req: Request, res: Response) => {
+//   const { endpoint, delay, method } = req.query;
+//   const { to, subject, text } = req.body;
+//   const user = req.user; 
+
+//   try {
+//       if (!user) {
+//           throw new Error("User not found");
+//       }
+
+//       const taskData: any = {
+//           endpoint: endpoint as string,
+//           delay: delay ? parseInt(delay as string) : 0,
+//           method: method as string,
+//           userId: user._id,
+//       };
+
+//       if (taskData.method.toLowerCase() !== "get") {
+//           if (!to || !subject || !text) {
+//               throw new Error("Missing required fields: to, subject, text");
+//           }
+//           taskData.data = { to, subject, text };
+//       }
+
+//       const task = new Task(taskData);
+
+//       await task.save();
+//       console.log("Task saved");
+
+//       const { responseData, taskStatus } = await executeTask(task);
+
+//       res.status(200).json({ responseData, status: taskStatus });
+//   } catch (err: any) {
+//       res.status(400).send(err.message);
+//   }
+// };
+
+
+
+// export const executeTask = async (task: ITask) => {
+//   try {
+//     await new Promise((resolve) => setTimeout(resolve, task.delay));
+
+//     const reqData: any = {
+//       method: task.method,
+//       url: task.endpoint,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     };
+
+//     if (task.method.toLowerCase() !== "get") {
+//       reqData.data = task.data;
+//     }
+
+//     const response: AxiosResponse = await axios(reqData);
+
+//     console.log(response.data, "response data");
+
+//     let taskStatus;
+//     if (response.status === 200) {
+//       task.status = "complete";
+//       taskStatus = "complete";
+//     } else {
+//       task.status = "failed";
+//       taskStatus = "failed";
+//     }
+
+//     await task.save();
+//     return { responseData: response.data, taskStatus };
+//   } catch (err) {
+//     console.error("Error executing task:", err);
+//     task.status = "failed";
+//     await task.save();
+//     return { responseData: null, taskStatus: "failed" };
+//   }
+// };
+
+// // get task status
+// export const getTasksByStatus = async (req: Request, res: Response) => {
+//   const { status } = req.params;
+
+//   try {
+//     const tasks = await Task.find({ status });
+//     res.status(200).json(tasks);
+//   } catch (err) {
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// };
+
+// //get task status by user token
+// export const getTasksByUserToken = async (req: Request, res: Response) => {
+//   try {
+//       const user = req.user; 
+
+//       if (!user) {
+//           throw new Error("User not found");
+//       }
+
+//       const tasks = await Task.find({ userId: user._id });
+
+//       res.status(200).json(tasks);
+//   } catch (err: any) {
+//       res.status(400).send(err.message);
+//   }
+// };
+
 import { Request, Response } from "express";
-import Task,{ITask} from "../models/Task";
-import axios, { AxiosResponse } from "axios";
+import Task, { ITask } from "../models/Task";
+import axios from "axios";
+import Agenda from "agenda";
 
-//task creation post endpoint
-export const createTask = async (req: Request, res: Response) => {
-  const { endpoint, delay, method } = req.query;
-  const { to, subject, text } = req.body;
-  const user = req.user; 
-
-  try {
-      if (!user) {
-          throw new Error("User not found");
-      }
-
-      const taskData: any = {
-          endpoint: endpoint as string,
-          delay: delay ? parseInt(delay as string) : 0,
-          method: method as string,
-          userId: user._id,
-      };
-
-      if (taskData.method.toLowerCase() !== "get") {
-          if (!to || !subject || !text) {
-              throw new Error("Missing required fields: to, subject, text");
-          }
-          taskData.data = { to, subject, text };
-      }
-
-      const task = new Task(taskData);
-
-      await task.save();
-      console.log("Task saved");
-
-      const { responseData, taskStatus } = await executeTask(task);
-
-      res.status(200).json({ responseData, status: taskStatus });
-  } catch (err: any) {
-      res.status(400).send(err.message);
-  }
-};
-
-
+const agenda = new Agenda({ db: { address: process.env.MONGODB_URI } });
 
 export const executeTask = async (task: ITask) => {
   try {
-    await new Promise((resolve) => setTimeout(resolve, task.delay));
-
-    const reqData: any = {
+    const response = await axios({
       method: task.method,
       url: task.endpoint,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-
-    if (task.method.toLowerCase() !== "get") {
-      reqData.data = task.data;
-    }
-
-    const response: AxiosResponse = await axios(reqData);
-
-    console.log(response.data, "response data");
+      headers: { "Content-Type": "application/json" },
+      data: task.data,
+    });
 
     let taskStatus;
     if (response.status === 200) {
@@ -81,7 +145,69 @@ export const executeTask = async (task: ITask) => {
   }
 };
 
-// get task status
+agenda.define("process task", async (job: any): Promise<void> => {
+  const task = await Task.findById(job.attrs.data.taskId);
+
+  if (!task) {
+    console.error("Task not found");
+    return null;
+  }
+
+  const { responseData, taskStatus } = await executeTask(task);
+
+  console.log(responseData,taskStatus,"stat")
+
+  if (taskStatus === "complete") {
+    console.log("Task is done");
+  }
+});
+
+(async () => {
+  await agenda.start();
+})();
+
+// Task creation endpoint
+export const createTask = async (req: Request, res: Response) => {
+  const { endpoint, delay, method } = req.query;
+  const { to, subject, text } = req.body;
+  const user = req.user;
+
+  try {
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const taskData: any = {
+      endpoint: endpoint as string,
+      delay: delay ? parseInt(delay as string) : 0,
+      method: method as string,
+      userId: user._id,
+    };
+
+    if (taskData.method.toLowerCase() !== "get") {
+      if (!to || !subject || !text) {
+        throw new Error("Missing required fields: to, subject, text");
+      }
+      taskData.data = { to, subject, text };
+    }
+
+    const task = new Task(taskData);
+
+    await task.save();
+    console.log("Task saved");
+
+    const scheduledTime = new Date(Date.now() + task.delay);
+    await agenda.schedule(scheduledTime, "process task", { taskId: task._id });
+
+    res.status(200).json({
+      message: "Task scheduled successfully",
+      taskId: task._id,
+    });
+  } catch (err: any) {
+    res.status(400).send(err.message);
+  }
+};
+
 export const getTasksByStatus = async (req: Request, res: Response) => {
   const { status } = req.params;
 
@@ -93,19 +219,18 @@ export const getTasksByStatus = async (req: Request, res: Response) => {
   }
 };
 
-//get task status by user token
 export const getTasksByUserToken = async (req: Request, res: Response) => {
   try {
-      const user = req.user; 
+    const user = req.user;
 
-      if (!user) {
-          throw new Error("User not found");
-      }
+    if (!user) {
+      throw new Error("User not found");
+    }
 
-      const tasks = await Task.find({ userId: user._id });
+    const tasks = await Task.find({ userId: user._id });
 
-      res.status(200).json(tasks);
+    res.status(200).json(tasks);
   } catch (err: any) {
-      res.status(400).send(err.message);
+    res.status(400).send(err.message);
   }
 };
